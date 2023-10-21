488bc1ca30a8db11a46775a456e5dc9c716ed253
Jens Erven <jens.erven@student.ehb.be>
2023-10-21 16:45:10 +0200

    fix(backend connection)

--- src/socket.js
+++ src/socket.js
@@ -1,9 +1,10 @@
 import { io } from "socket.io-client";
 
-// "undefined" means the URL will be computed from the `window.location` object
+const productionURL =
+  "https://expertlab-prototype-live-chat-application-socket-io-backend.vercel.app/";
+const developmentURL = "http://localhost:3001";
+
 const URL =
-  process.env.NODE_ENV === "production"
-    ? undefined
-    : "expertlab-prototype-live-chat-application-socket-io-backend.vercel.app";
+  process.env.NODE_ENV === "production" ? productionURL : developmentURL;
 
 export const socket = io(URL);
